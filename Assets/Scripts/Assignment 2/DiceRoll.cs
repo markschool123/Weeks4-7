f545675252dd098f531f822a0789f56707e9286f
ferna100 <ferna100@OA-S141-05>
2026-02-10 11:44:58 -0500

    added random roll for die

--- Assets/Scripts/Assignment 2/DiceRoll.cs
+++ Assets/Scripts/Assignment 2/DiceRoll.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class DiceRoll : MonoBehaviour
 {
+    public int roll;
+    public SpriteRenderer die;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -10,7 +13,9 @@ public class DiceRoll : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {    
+
+        roll = Random.Range(0, 5);
+
     }
 }

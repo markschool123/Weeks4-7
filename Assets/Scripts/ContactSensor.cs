459ac3a58448636a655b423d7587ad5cb2e853d2
ferna100 <ferna100@OA-S141-03>
2026-02-17 10:49:29 -0500

    Contact senros code working

--- Assets/Scripts/ContactSensor.cs
+++ Assets/Scripts/ContactSensor.cs
@@ -1,7 +1,14 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ContactSensor : MonoBehaviour
 {
+    public SpriteRenderer hazard;
+    public bool isInHazard = false;
+    public UnityEvent OnEnterHazard;
+    public UnityEvent OnExitHazard;
+
+    public UnityEvent<float> OnRandomNumber;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +18,38 @@ public class ContactSensor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (hazard.bounds.Contains(transform.position) ==true)
+        {
+            if (isInHazard ==true)
+            {
+
+            }
+            else
+            {
+                isInHazard = true;
+                Debug.Log("Entered the Hazard!");
+                OnEnterHazard.Invoke();
+                OnRandomNumber.Invoke(Random.Range(0, 10));
+            }
+        }
+        else
+        {
+            if (isInHazard ==true)
+            {
+                isInHazard=false;
+                Debug.Log("Exited the Hazard");
+                OnExitHazard.Invoke();
+            }
+            else
+            {
+
+            }
+
+        }
+    }
+
+    public void ShowNumber(float number)
+    {
+        Debug.Log(number);
     }
 }
